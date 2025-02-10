3593916e33af1b35c405afb635a15b3920f6b6ee
Mukesh <hey.mukesh272921@gmail.com>
2025-02-10 20:42:24 +0530

    ...

--- datasets/spiral.go
+++ datasets/spiral.go
@@ -1,10 +1,13 @@
 package datasets
 
 import (
+	"encoding/csv"
 	"math"
 	"math/rand/v2"
 	m "nn/math"
 	"nn/utils"
+	"os"
+	"strconv"
 )
 
 func NewSpiralData(numberOfPoints, numberOfClasses int) (m.Matrix, m.Matrix) {
@@ -27,4 +30,112 @@ func NewSpiralData(numberOfPoints, numberOfClasses int) (m.Matrix, m.Matrix) {
 	}
 
 	return X, Y
+}
+
+func SaveSpiralDataset(filename string, X, Y m.Matrix, trainRatio float64) error {
+	if trainRatio <= 0 || trainRatio >= 1 {
+		trainRatio = 0.8
+	}
+
+	totalSamples := len(X)
+	trainSamples := int(float64(totalSamples) * trainRatio)
+
+	indices := make([]int, totalSamples)
+	for i := range indices {
+		indices[i] = i
+	}
+	rand.Shuffle(len(indices), func(i, j int) {
+		indices[i], indices[j] = indices[j], indices[i]
+	})
+
+	trainFile, err := os.Create(filename + "_train.csv")
+	if err != nil {
+		return err
+	}
+	defer trainFile.Close()
+
+	testFile, err := os.Create(filename + "_test.csv")
+	if err != nil {
+		return err
+	}
+	defer testFile.Close()
+
+	trainWriter := csv.NewWriter(trainFile)
+	testWriter := csv.NewWriter(testFile)
+	defer trainWriter.Flush()
+	defer testWriter.Flush()
+
+	header := []string{"x1", "x2"}
+	for i := range Y[0] {
+		header = append(header, "class_"+strconv.Itoa(i))
+	}
+	if err := trainWriter.Write(header); err != nil {
+		return err
+	}
+	if err := testWriter.Write(header); err != nil {
+		return err
+	}
+
+	for i := 0; i < trainSamples; i++ {
+		row := []string{
+			strconv.FormatFloat(X[indices[i]][0], 'f', 6, 64),
+			strconv.FormatFloat(X[indices[i]][1], 'f', 6, 64),
+		}
+		for j := range Y[indices[i]] {
+			row = append(row, strconv.FormatFloat(Y[indices[i]][j], 'f', 6, 64))
+		}
+		if err := trainWriter.Write(row); err != nil {
+			return err
+		}
+	}
+
+	for i := trainSamples; i < totalSamples; i++ {
+		row := []string{
+			strconv.FormatFloat(X[indices[i]][0], 'f', 6, 64),
+			strconv.FormatFloat(X[indices[i]][1], 'f', 6, 64),
+		}
+		for j := range Y[indices[i]] {
+			row = append(row, strconv.FormatFloat(Y[indices[i]][j], 'f', 6, 64))
+		}
+		if err := testWriter.Write(row); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func LoadSpiralDataset(filename string) (X, Y m.Matrix, err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	records = records[1:]
+
+	X = m.AllocateMatrix(len(records), 2)
+	Y = m.AllocateMatrix(len(records), len(records[0])-2)
+
+	for i, record := range records {
+		X[i][0], _ = strconv.ParseFloat(record[0], 64)
+		X[i][1], _ = strconv.ParseFloat(record[1], 64)
+
+		for j := 2; j < len(record); j++ {
+			Y[i][j-2], _ = strconv.ParseFloat(record[j], 64)
+		}
+	}
+
+	return X, Y, nil
+}
+
+func GenerateAndSaveSpiralDataset(filename string, points, classes int) error {
+	X, Y := NewSpiralData(points, classes)
+	return SaveSpiralDataset(filename, X, Y, 0.8)
 }
--- main.go
+++ main.go
@@ -8,17 +8,33 @@ import (
 	"nn/neural/losses"
 	"nn/neural/optimizers"
 	"nn/utils"
+	"os"
 )
 
 func main() {
-	X, Y := datasets.NewSpiralData(100, 3)
+	cmd := os.Args[len(os.Args)-1]
+
+	if cmd == "generate" {
+		if err := datasets.GenerateAndSaveSpiralDataset("data/spiral_dataset", 200, 3); err != nil {
+			fmt.Printf("an error occurred while generating the dataset - %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		return
+	}
+
+	X, Y, err := datasets.LoadSpiralDataset("data/spiral_dataset_train.csv")
+	if err != nil {
+		fmt.Printf("an error occurred while loading the dataset - %s\n", err.Error())
+		os.Exit(1)
+	}
 
 	dl1 := neural.NewDenseLayer(2, 64)
 	relu := activations.NewRelu()
 	dl2 := neural.NewDenseLayer(64, 3)
 	softmax := activations.NewSoftmax()
 	ccel := losses.NewCcel()
-	sgdMomentum := optimizers.NewSGDMomentum(1.0, 0.01, 0.7)
+	sgdMomentum := optimizers.NewSGDMomentum(0.4, 0.01, 0.9)
 
 	for i := range 10_001 {
 		dl1.Forward(X)
